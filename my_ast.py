d61f7c914f26db8da5c736e0510a4f467936e308
SantiagoSauma <lssaumap@gmail.com>
2024-07-09 15:31:14 -0400

    Fixed while loop error

--- my_ast.py
+++ my_ast.py
@@ -8,7 +8,7 @@ class Number:
         self.value = value
 
     def eval(self, context):
-        return int(self.value)
+        return ir.Constant(ir.IntType(32), int(self.value))
 
 class Identifier:
     def __init__(self, builder, module, name):
@@ -26,35 +26,204 @@ class BinaryOp:
         self.left = left
         self.right = right
 
+    def eval_operands(self, context):
+        left_val = self.left.eval(context)
+        right_val = self.right.eval(context)
+        
+        # Ensure both operands are LLVM IR values
+        if not isinstance(left_val, ir.Value):
+            left_val = ir.Constant(ir.IntType(32), left_val)
+        if not isinstance(right_val, ir.Value):
+            right_val = ir.Constant(ir.IntType(32), right_val)
+
+        return left_val, right_val
+
 class Sum(BinaryOp):
     def eval(self, context):
-        return self.builder.add(self.left.eval(context), self.right.eval(context))
+        left_val, right_val = self.eval_operands(context)
+        return self.builder.add(left_val, right_val)
 
 class Sub(BinaryOp):
     def eval(self, context):
-        return self.builder.sub(self.left.eval(context), self.right.eval(context))
+        left_val, right_val = self.eval_operands(context)
+        return self.builder.sub(left_val, right_val)
 
 class Mul(BinaryOp):
     def eval(self, context):
-        return self.builder.mul(self.left.eval(context), self.right.eval(context))
+        left_val, right_val = self.eval_operands(context)
+        return self.builder.mul(left_val, right_val)
 
 class Div(BinaryOp):
     def eval(self, context):
-        return self.builder.sdiv(self.left.eval(context), self.right.eval(context))
+        left_val, right_val = self.eval_operands(context)
+        return self.builder.sdiv(left_val, right_val)
 
 class Mod(BinaryOp):
     def eval(self, context):
-        return self.builder.srem(self.left.eval(context), self.right.eval(context))
+        left_val, right_val = self.eval_operands(context)
+        return self.builder.srem(left_val, right_val)
 
 class Pow(BinaryOp):
     def eval(self, context):
-        left_val = self.left.eval(context)
-        right_val = self.right.eval(context)
+        left_val, right_val = self.eval_operands(context)
         left_float = self.builder.uitofp(left_val, ir.DoubleType())
         right_float = self.builder.uitofp(right_val, ir.DoubleType())
         pow_func = self.module.declare_intrinsic('llvm.pow', [ir.DoubleType()])
         result = self.builder.call(pow_func, [left_float, right_float])
         return self.builder.fptoui(result, ir.IntType(32))
+
+class If:
+    def __init__(self, builder, module, printf, condition, body):
+        self.builder = builder
+        self.module = module
+        self.printf = printf
+        self.condition = condition
+        self.body = body
+
+    def eval(self, context):
+        cond = self.condition.eval(context)
+        then_block = self.builder.append_basic_block('then')
+        endif_block = self.builder.append_basic_block('endif')
+        
+        self.builder.cbranch(cond, then_block, endif_block)
+        
+        self.builder.position_at_end(then_block)
+        for stmt in self.body:
+            stmt.eval(context)
+        self.builder.branch(endif_block)
+        
+        self.builder.position_at_end(endif_block)
+
+class While:
+    def __init__(self, builder, module, printf, condition, body):
+        self.builder = builder
+        self.module = module
+        self.printf = printf
+        self.condition = condition
+        self.body = body
+
+    def eval(self, context):
+        loop_block = self.builder.append_basic_block('loop')
+        after_loop_block = self.builder.append_basic_block('afterloop')
+        
+        self.builder.branch(loop_block)
+        self.builder.position_at_end(loop_block)
+        
+        cond = self.condition.eval(context)
+        with self.builder.if_then(cond):
+            for stmt in self.body:
+                stmt.eval(context)
+            self.builder.branch(loop_block)
+        
+        self.builder.branch(after_loop_block)
+        self.builder.position_at_end(after_loop_block)
+
+class List:
+    def __init__(self, elements):
+        self.elements = elements
+
+    def eval(self, context):
+        evaluated_elements = []
+        for element in self.elements:
+            eval_element = element.eval(context)
+            if isinstance(eval_element, ir.Constant):
+                evaluated_elements.append(int(eval_element.constant))
+            else:
+                evaluated_elements.append(eval_element)
+        return np.array(evaluated_elements, dtype=np.int32)
+
+class ListAccess:
+    def __init__(self, name, index):
+        self.name = name
+        self.index = index
+
+    def eval(self, context):
+        array = context[self.name]
+        index = self.index.eval(context)
+        return array[index]
+
+class ListAssign:
+    def __init__(self, name, index, value):
+        self.name = name
+        self.index = index
+        self.value = value
+
+    def eval(self, context):
+        array = context[self.name]
+        index = self.index.eval(context)
+        value = self.value.eval(context)
+        array[index] = value
+
+class Assign:
+    def __init__(self, builder, module, name, value):
+        self.builder = builder
+        self.module = module
+        self.name = name
+        self.value = value
+
+    def eval(self, context):
+        value = self.value.eval(context)
+        if isinstance(value, np.ndarray):
+            # Handle numpy arrays separately
+            array_type = ir.ArrayType(ir.IntType(32), len(value))
+            array_constant = ir.Constant(array_type, [ir.Constant(ir.IntType(32), int(v)) for v in value])
+            if self.name not in self.module.globals:
+                var = ir.GlobalVariable(self.module, array_type, self.name)
+                var.linkage = 'internal'
+                var.global_constant = False
+                var.initializer = array_constant
+                context[self.name] = value
+            else:
+                var = self.module.globals[self.name]
+                self.builder.store(array_constant, var)
+                context[self.name] = value
+        else:
+            # Handle scalar values
+            if not isinstance(value, ir.Value):
+                value = ir.Constant(ir.IntType(32), value)
+
+            if self.name not in self.module.globals:
+                var = ir.GlobalVariable(self.module, value.type, self.name)
+                var.initializer = ir.Constant(value.type, None)
+                var.linkage = 'internal'
+                var.global_constant = False
+                context[self.name] = value
+            else:
+                var = self.module.globals[self.name]
+
+            self.builder.store(value, var)
+            context[self.name] = value
+
+class Condition:
+    def __init__(self, builder, module, left, right, operator):
+        self.builder = builder
+        self.module = module
+        self.left = left
+        self.right = right
+        self.operator = operator
+
+    def eval(self, context):
+        left_val = self.left.eval(context)
+        right_val = self.right.eval(context)
+
+        # Ensure both operands are LLVM IR values
+        if not isinstance(left_val, ir.Value):
+            left_val = ir.Constant(ir.IntType(32), left_val)
+        if not isinstance(right_val, ir.Value):
+            right_val = ir.Constant(ir.IntType(32), right_val)
+
+        if self.operator == 'EQ':
+            return self.builder.icmp_signed('==', left_val, right_val)
+        elif self.operator == 'NEQ':
+            return self.builder.icmp_signed('!=', left_val, right_val)
+        elif self.operator == 'GT':
+            return self.builder.icmp_signed('>', left_val, right_val)
+        elif self.operator == 'LT':
+            return self.builder.icmp_signed('<', left_val, right_val)
+        elif self.operator == 'GTE':
+            return self.builder.icmp_signed('>=', left_val, right_val)
+        elif self.operator == 'LTE':
+            return self.builder.icmp_signed('<=', left_val, right_val)
 
 class String:
     def __init__(self, builder, module, value):
--- output.ll
+++ output.ll
@@ -5,71 +5,27 @@ target datalayout = ""
 define void @"main"()
 {
 entry:
-  store i32 5, i32* @"a"
-  store i32 3, i32* @"b"
-  %".4" = load i32, i32* @"a"
-  %".5" = load i32, i32* @"b"
-  %".6" = add i32 %".4", %".5"
-  store i32 %".6", i32* @"suma"
-  %".8" = load i32, i32* @"suma"
-  %".9" = bitcast [5 x i8]* @"fstr0" to i8*
-  %".10" = call i32 (i8*, ...) @"printf"(i8* %".9", i32 %".8")
-  %".11" = load i32, i32* @"a"
-  %".12" = load i32, i32* @"b"
-  %".13" = sub i32 %".11", %".12"
-  store i32 %".13", i32* @"resta"
-  %".15" = load i32, i32* @"resta"
-  %".16" = bitcast [5 x i8]* @"fstr1" to i8*
-  %".17" = call i32 (i8*, ...) @"printf"(i8* %".16", i32 %".15")
-  %".18" = load i32, i32* @"a"
-  %".19" = load i32, i32* @"b"
-  %".20" = mul i32 %".18", %".19"
-  store i32 %".20", i32* @"multiplicacion"
-  %".22" = load i32, i32* @"multiplicacion"
-  %".23" = bitcast [5 x i8]* @"fstr2" to i8*
-  %".24" = call i32 (i8*, ...) @"printf"(i8* %".23", i32 %".22")
-  %".25" = load i32, i32* @"a"
-  %".26" = load i32, i32* @"b"
-  %".27" = sdiv i32 %".25", %".26"
-  store i32 %".27", i32* @"division"
-  %".29" = load i32, i32* @"division"
-  %".30" = bitcast [5 x i8]* @"fstr3" to i8*
-  %".31" = call i32 (i8*, ...) @"printf"(i8* %".30", i32 %".29")
-  %".32" = load i32, i32* @"a"
-  %".33" = load i32, i32* @"b"
-  %".34" = srem i32 %".32", %".33"
-  store i32 %".34", i32* @"modulo"
-  %".36" = load i32, i32* @"modulo"
-  %".37" = bitcast [5 x i8]* @"fstr4" to i8*
-  %".38" = call i32 (i8*, ...) @"printf"(i8* %".37", i32 %".36")
-  %".39" = load i32, i32* @"a"
-  %".40" = load i32, i32* @"b"
-  %".41" = uitofp i32 %".39" to double
-  %".42" = uitofp i32 %".40" to double
-  %".43" = call double @"llvm.pow.f64"(double %".41", double %".42")
-  %".44" = fptoui double %".43" to i32
-  store i32 %".44", i32* @"potencia"
-  %".46" = load i32, i32* @"potencia"
-  %".47" = bitcast [5 x i8]* @"fstr5" to i8*
-  %".48" = call i32 (i8*, ...) @"printf"(i8* %".47", i32 %".46")
+  store i32 0, i32* @"i"
+  br label %"loop"
+loop:
+  %".4" = load i32, i32* @"i"
+  %".5" = icmp slt i32 %".4", 5
+  br i1 %".5", label %"loop.if", label %"loop.endif"
+afterloop:
   ret void
+loop.if:
+  %".7" = load i32, i32* @"i"
+  %".8" = bitcast [5 x i8]* @"fstr0" to i8*
+  %".9" = call i32 (i8*, ...) @"printf"(i8* %".8", i32 %".7")
+  %".10" = load i32, i32* @"i"
+  %".11" = add i32 %".10", 1
+  store i32 %".11", i32* @"i"
+  br label %"loop"
+loop.endif:
+  br label %"afterloop"
 }
 
 declare i32 @"printf"(i8* %".1", ...)
 
-@"a" = internal global i32 0
-@"b" = internal global i32 0
-@"suma" = internal global i32 0
+@"i" = internal global i32 0
 @"fstr0" = internal constant [5 x i8] c"%i \0a\00"
-@"resta" = internal global i32 0
-@"fstr1" = internal constant [5 x i8] c"%i \0a\00"
-@"multiplicacion" = internal global i32 0
-@"fstr2" = internal constant [5 x i8] c"%i \0a\00"
-@"division" = internal global i32 0
-@"fstr3" = internal constant [5 x i8] c"%i \0a\00"
-@"modulo" = internal global i32 0
-@"fstr4" = internal constant [5 x i8] c"%i \0a\00"
-declare double @"llvm.pow.f64"(double %".1", double %".2")
-
-@"potencia" = internal global i32 0
-@"fstr5" = internal constant [5 x i8] c"%i \0a\00"
